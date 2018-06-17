1acbbd6b8d94b20d6471fb5848c891b119878e50
George Chamoun <gchamounster123@gmail.com>
2018-06-17 18:30:54 -0500

    Register Page

--- app/src/main/java/com/libnanman/liftingplanner/Register.java
+++ app/src/main/java/com/libnanman/liftingplanner/Register.java
@@ -1,6 +1,7 @@
 package com.libnanman.liftingplanner;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,9 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -17,14 +21,17 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.lang.Object;
+
 public class Register extends AppCompatActivity {
-private EditText mEmailCreateField;
-private EditText mPswCreateField;
-private EditText mNameField;
-private Button mCreateBtn;
-private FirebaseAuth mAuth;
-private DatabaseReference mDatabase;
-private ProgressDialog mProgress;
+    private Button mRegister;
+    private EditText mEmail, mPassword, mName, mWeight, mHeight, mAge;
+    private RadioGroup mRadioGroup;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener fireAuthStateListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,49 +39,83 @@ private ProgressDialog mProgress;
         setContentView(R.layout.activity_register);
 
         mAuth = FirebaseAuth.getInstance();
-        mProgress = new ProgressDialog(this);
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
-
-        mEmailCreateField = (EditText) findViewById(R.id.emailCreateField);
-        mPswCreateField = (EditText) findViewById(R.id.pswCreateField);
-        mCreateBtn = (Button) findViewById(R.id.createBtn);
-        mNameField = (EditText) findViewById(R.id.nameField);
-
-        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+        fireAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void onClick(View view) {
-                startRegister();
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if(user != null){
+                    Intent intent =  new Intent(Register.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
+            }
+        };
+
+        mRegister = (Button) findViewById(R.id.register);
+        mEmail = (EditText) findViewById(R.id.email);
+        mPassword = (EditText) findViewById(R.id.password);
+        mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        mName = (EditText) findViewById(R.id.name);
+        mHeight = (EditText) findViewById(R.id.height);
+        mWeight = (EditText) findViewById(R.id.weight);
+        mAge = (EditText) findViewById(R.id.age);
+
+
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectId = mRadioGroup.getCheckedRadioButtonId();
+
+                final RadioButton radioButton = (RadioButton) findViewById(selectId);
+
+                if(radioButton.getText() == null){
+                    return;
+                }
+                final Integer weight = Integer.parseInt(mWeight.getText().toString());
+                final Integer height = Integer.parseInt(mHeight.getText().toString());
+                final Integer age = Integer.parseInt(mAge.getText().toString());
+
+                final String email = mEmail.getText().toString();
+                final String password = mPassword.getText().toString();
+                final String name = mName.getText().toString();
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(Register.this, "sign_up_error", Toast.LENGTH_SHORT).show();
+                        }else{
+                            String Gender = radioButton.getText().toString();
+
+                            User user = new User(name, age,height,weight, Gender);
+                            writeUserData(user);
+                        }
+                    }
+                });
+
             }
         });
     }
 
-    private void startRegister() {
-        final String email = mEmailCreateField.getText().toString();
-        String password = mPswCreateField.getText().toString();
+    protected void writeUserData(User user){
+        String userId = mAuth.getCurrentUser().getUid();
+        DatabaseReference currentUserDb = FirebaseDatabase.getInstance().
+                getReference().child("Users").child(userId);
+        currentUserDb.setValue(user);
 
-        if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
-            mProgress.setMessage("Signing up...");
-            mProgress.show();
+    }
 
-            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(task.isSuccessful()){
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(fireAuthStateListener);
+    }
 
-                        FirebaseDatabase database = FirebaseDatabase.getInstance();
-                        FirebaseAuth.getInstance()
-                        DatabaseReference myRef = database.getReference("message");
-
-                        myRef.setValue("Hello, World!");
-
-
-                    }
-                }
-            });
-
-
-
-
-        }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(fireAuthStateListener);
     }
 }
